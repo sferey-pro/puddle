d87d26718ea4048fb05be312c820375b796c9efa
sferey-pro <sferey.pro@gmail.com>
2025-03-05 16:51:15 +0100

    fix: changement url homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -7,11 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomepageController extends AbstractController{
-    #[Route('/homepage', name: 'app_homepage')]
+
+    #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
     }
+
 }
