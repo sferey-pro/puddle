c2901fedbd33f382e1e47587ac0e2f12154367eb
sferey-pro <sferey.pro@gmail.com>
2025-03-05 22:34:56 +0100

    feat: Passage au symfony CLI feat: Transformation en Invoke class le controller Registration et VerifyEmail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,22 +8,15 @@ use App\Common\Command\CommandBusInterface;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Messenger\Command\User\RegisterUser;
-use App\Security\EmailVerifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
+#[Route('/register', name: 'app_register')]
 class RegistrationController extends AbstractController
 {
-    public function __construct(
-        private EmailVerifier $emailVerifier,
-    ) {
-    }
-
-    #[Route('/register', name: 'app_register')]
-    public function register(
+    public function __invoke(
         Request $request,
         CommandBusInterface $commandBus,
     ): Response {
@@ -42,26 +35,5 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
-    }
-
-    #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request): Response
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            /** @var User $user */
-            $user = $this->getUser();
-            $this->emailVerifier->handleEmailConfirmation($request, $user);
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
-
-            return $this->redirectToRoute('app_register');
-        }
-
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_register');
     }
 }
---
+++ src/Controller/VerifyEmailController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Common\Query\QueryBusInterface;
+use App\Entity\User;
+use App\Security\EmailVerifier;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+
+#[Route('/verify/email', name: 'app_verify_email')]
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+class VerifyEmailController extends AbstractController
+{
+    public function __construct(
+        private EmailVerifier $emailVerifier,
+    ) {
+    }
+
+    public function __invoke(
+        Request $request,
+    ): Response {
+        try {
+            /** @var User $user */
+            $user = $this->getUser();
+            $this->emailVerifier->handleEmailConfirmation($request, $user);
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $exception->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_register');
+    }
+}
