6bf74798914d0c23081cf480e0342799f09e7740
sferey-pro <sferey.pro@gmail.com>
2025-03-06 16:35:07 +0100

    feat: templating login page feat: Ajout du Darkmode feat: Ajout d'icone

--- assets/app.js
+++ assets/app.js
@@ -2,9 +2,8 @@
 import './bootstrap.js';
 import './styles/app.scss';
 
-// loads the Bootstrap plugins
-import 'bootstrap/js/dist/alert';
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/tab';
-import 'bootstrap/js/dist/modal';
+// loads the Bootstrap
+import 'bootstrap';
+
+// loads the code color-mode
+import './js/color-modes.js';
